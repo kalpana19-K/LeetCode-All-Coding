05eb9fa1b44116691a6507b16f4c0ff100316fe3
kalpana19-K <kalpana.biswakalyani@gmail.com>
2025-10-18 13:23:33 +0530

    Time: 32 ms (38.88%), Space: 51.6 MB (56.23%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,38 @@
 class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set = new HashSet <>();  //HashSet for unique triplet
-       int n = nums.length;
+        List <List <Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3 ) return result;
 
-       for (int i =0; i< n-2; i++){
-        for (int j = i+1; j < n-1 ; j++){
-            for (int k= j+1; k<n;k++ ){
-                if (nums[i] + nums [j] + nums[k] == 0){
-                    List <Integer> triplet = Arrays.asList (nums [i] , nums [j] ,nums [k]);
-                    Collections.sort(triplet); //sort to handle duplicate like [0.1.-1] == [-1,0,1]
-                   set.add(triplet);  //Hashset automatically ignore duplicate
-                    }
+        Arrays.sort (nums);
+        for (int i = 0; i < nums.length-2; i++){
+            if(i > 0 && nums[i] == nums[i-1])continue;
+
+            int left = i+1;
+            int right = nums.length-1;
+
+            while (left<right) {
+                int sum = nums [i] + nums [left] + nums [right];
+
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i] , nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums [left+1]) left++;
+
+                    while(left < right && nums[right] == nums [right-1]) right--;
+
+                    left++;
+                    right--;  
+                }else if(sum < 0){
+                    left++;
+                }else {
+                    right--;
                 }
             }
-         }  
-      
-      return new ArrayList<>(set);  //convert back to List
-  }
-  //main function for adding
-  public static void main (String [] args){
-    int[] nums = {-1,0,1,2,-1,-4};
-    List <List <Integer>> ans= threeSum (nums);
-    System.out.println(ans);  //output= [-1 ,0,1] [-1,0,1]
-  }
+        }
+        return result;
+    }
+    public static void main (String[] args){
+        int[] nums = {-1, 0, 1, 2, -1, -1,};
+        System.out.println(threeSum(nums));
+    }
 }
