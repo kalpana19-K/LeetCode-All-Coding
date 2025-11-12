6fbeb18feedf4d7865516e4d206c7a8cf31bef0f
kalpana19-K <kalpana.biswakalyani@gmail.com>
2025-11-12 07:31:38 +0530

    Time: 0 ms (100%), Space: 43.6 MB (11.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,24 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        PriorityQueue<Integer>pq = new  PriorityQueue<>();
-
-        //Add first m element of nums1
-
-        for(int i = 0; i<m; i++){
-            pq.add(nums1[i]);
+        int p1 = m-1;
+        int p2 = n-1;
+        int p = m+n-1;
+        
+        while(p2 >= 0){
+            if(p1 >= 0 && nums1[p1] > nums2[p2]){
+            
+            nums1[p] = nums1[p1];
+            p1--;
+        }else{
+            nums1[p] = nums2[p2];
+            p2--;
         }
-
-        //Add all elements of nums2 
-         for(int i = 0 ; i<n ; i++){
-            pq.add(nums2 [i]);
-        
-         }
-         //Refill nums1 in sorted order 
-         int i= 0;
-         while (!pq .isEmpty()){
-            nums1[i++] = pq.poll();
-
-         }
-        
+        p--;
+        }
     }
 }
